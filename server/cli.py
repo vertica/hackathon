4a17c8a2ef3dab81531b08cf136f0c9b31a03f99
Matthew Saltz <matthew.saltz@hpe.com>
2016-10-01 20:15:58 +0000

    Add cli for easier command line use

---
+++ server/cli.py
@@ -0,0 +1,50 @@
+
+def get_db():
+    import vertica_python
+
+    import re
+    import os
+
+    DB_NAME = os.environ['DB_NAME']
+    if not DB_NAME:
+        DB_NAME = 'test'
+    try:
+        DB_USER = os.environ['DB_USER']
+    except Exception, e:
+        DB_USER = 'dbadmin'
+    DB_PASSWORD = ''
+    DB_HOST = os.environ['DB_HOST']
+
+    conn_info = {'host': DB_HOST,
+                 'port': 5433,
+                 'user': DB_USER,
+                 'password': '',
+                 'database': DB_NAME,
+                 # 10 minutes timeout on queries
+                 'read_timeout': 600,
+                 # default throw error on invalid UTF-8 results
+                 'unicode_error': 'strict',
+                 # SSL is disabled by default
+                 'ssl': False}
+
+    db = vertica_python.connect(**conn_info)
+    return db
+
+def query_db(query, args=(), one=False):
+    print query
+    cur = get_db().cursor()
+
+    try:
+        cur.execute(query, args)
+        rv = cur.fetchall()
+
+        # Turn into colname->val dict representation of tuple
+        # this isn't very efficient but will suffice for now
+#        rv = [make_dicts(cur, row) for row in rv]
+    except Exception, e:
+        print e
+#        rv = [{'error': e}]
+
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
