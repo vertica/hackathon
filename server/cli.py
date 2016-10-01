d97cd7c8acf5a791dbb7d3b26a4354f77c60d759
Matthew Saltz <matthew.saltz@hpe.com>
2016-10-01 22:23:47 +0000

    Fixed bug where empty lines threw exception

--- server/cli.py
+++ server/cli.py
@@ -6,11 +6,13 @@ if __name__ == "__main__":
 
     print "Welcome to the mini Vertica client! Enter your queries or type '\\q' to quit"
     line = raw_input("> ")
-    while not line == "\\q":
-        while not line[-1] == ";":
-            line += raw_input("(cont)> ")
-        if line.split(' ')[0] == "insert":
-            line += "; commit;"
-        query_db(line, pretty_print=True)
+    while not line.strip() == "\\q":
+        if len(line):
+            while not line[-1] == ";":
+                line += raw_input("(cont)> ")
+            if line.split(' ')[0] == "insert":
+                line += "; commit;"
+
+            query_db(line, pretty_print=True)
         line = raw_input("> ")
 
